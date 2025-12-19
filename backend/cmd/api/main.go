8a4f2322a45f55fc133b1d9a560af8ab98fc2677
MrDrtyui <drtyuicode@gmail.com>
2025-12-19 19:48:50 +0300

    Rename api to app

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"app/internal/app/api"
+	"app/internal/app/app"
 )
 
 func main() {
 	// TODO: init app
-	if err := api.Run(); err != nil {
+	if err := app.Run(); err != nil {
 		log.Fatal("Failed to run server http", err.Error())
 	}
 }
